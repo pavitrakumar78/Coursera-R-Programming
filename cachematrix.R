1ccc6c19386233d79674512a351387caa396a1e5
pavitrakumar78 <pavitrakumar78@gmail.com>
2014-12-13 17:12:29 +0530

    added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -7,12 +7,16 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
+        # function that sets the given matrix 'y' to 'x'
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        # function to retrieve the exisiting matrix
         get <- function() { x }
+        # function to set the inverse of the matrix to 'm'
         setInverse <- function(inv) { m <<- inv }
+        # function to return the inverse of matrix i.e 'm'
         getInverse <- function() { m }
         list(set = set, get = get,
              setInverse = setInverse,
@@ -25,14 +29,28 @@ makeCacheMatrix <- function(x = matrix()) {
 ## else retrieves the already calculated inverse from the cache and returns it.
 
 cacheInverse <- function(x, ...) {
+        # 'm' now holds the inverse (if it exists) returned by the getInverse() function
         m <- x$getInverse()
         if(!is.null(m)) {
+                # if inverse is already calculated, then return it
                 message("getting inverse-ed matrix")
                 return(m)
         }
+        # else, get the original matrix using the get() function
         data <- x$get()
+        # calculate the inverse using R's solve() method
+        message("calculating inverse...")
         m <- solve(data, ...)
+        # set the inverse using setInverse() method
         x$setInverse(m)
         m
 }
 
+#EXAMPLE:
+x = matrix(c(1,2,3,4,11,34,78,8,9),ncol = 3,nrow = 3)
+m_c <- makeCacheMatrix(x)
+# first call to cache the matrix, the inverse is created in this call
+cacheInverse(m_c)
+# now the nex time we want to use the inverse, we don't need to calculate it again
+# since it was already calculated, the existing inverse is returned
+cacheInverse(m_c)
